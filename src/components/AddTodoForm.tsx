156925de2b9676636d68ef93ae1461bce17a718c
herman <hermanconnor11@gmail.com>
2024-01-03 22:06:22 -0500

    Install nanoid and classnames, and style form input and button

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -2,11 +2,22 @@ const AddTodoForm = () => {
   return (
     <form>
       <div className="m-auto flex w-full max-w-lg items-center gap-2 p-6">
+        <label htmlFor="todoInput" className="sr-only">
+          Add a Todo
+        </label>
         <input
           type="text"
+          id="todoInput"
           placeholder="Enter something to do..."
-          className="w-full rounded-xl border-2 border-zinc-600 px-5 py-2"
+          className="w-full rounded-xl border-2 border-zinc-600 bg-transparent px-5 py-2 outline-none transition-all duration-150 ease-linear placeholder:text-zinc-500 focus:border-indigo-500 focus:ring-4 focus:ring-indigo-500/50"
         />
+
+        <button
+          type="submit"
+          className="rounded-full bg-indigo-500 px-5 py-2 uppercase leading-normal text-white transition-all duration-150 ease-in-out hover:bg-indigo-600 hover:shadow-md focus:bg-indigo-600 focus:shadow-lg focus:outline-none focus:ring focus:ring-indigo-300 active:bg-indigo-700"
+        >
+          Submit
+        </button>
       </div>
     </form>
   );
