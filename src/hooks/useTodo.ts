95c521696d90fc50cab54b6ef3e4edc4d5e17d02
herman <hermanconnor11@gmail.com>
2024-01-06 00:02:01 -0500

    Create useTodo hook

---
+++ src/hooks/useTodo.ts
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+
+import { TodoContext } from "@/context/TodoProvider";
+
+const useTodo = () => {
+  const context = useContext(TodoContext);
+
+  if (!context) {
+    throw new Error("useTodo must be used within a TodoProvider");
+  }
+
+  return context;
+};
+
+export default useTodo;
