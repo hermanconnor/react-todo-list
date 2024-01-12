30ae66c4842bb8ec47422d54b9ec7f3ca94ab83e
herman <hermanconnor11@gmail.com>
2024-01-11 22:50:56 -0500

    Add svg image if there are no todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,24 @@
 import { motion } from "framer-motion";
 
-import TodoItem from "@/components/TodoItem";
+import { TodoItem, NothingToDo } from "@/components";
+
 import useTodo from "@/hooks/useTodo";
 
 const TodoList = () => {
   const { todos } = useTodo();
+
+  if (!todos.length) {
+    return (
+      <div className="m-auto max-w-lg px-5">
+        <div className="flex flex-col items-center gap-5 rounded-xl px-5 py-10 text-center text-xl font-bold">
+          <h2>You have nothing to do!</h2>
+
+          <NothingToDo />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <motion.ul className="m-auto grid max-w-lg gap-2 px-5">
       {todos.map((todo) => (
