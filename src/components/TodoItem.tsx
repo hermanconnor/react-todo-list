4f6721628b6f9c6d1cb67066c08f44b7750d5fc6
herman <hermanconnor11@gmail.com>
2024-01-11 22:32:00 -0500

    Add icons to status toggle button

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -90,7 +90,45 @@ const TodoItem = ({ todo }: Props) => {
           </motion.span>
 
           <div className="flex justify-between gap-5 text-white">
-            <button className="">Update complete button</button>
+            <button onClick={() => handleStatusUpdate(todo.id)}>
+              {todo.status === "incomplete" ? (
+                <span className="flex items-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  Mark Completed
+                </span>
+              ) : (
+                <span className="flex items-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                  </svg>
+                  Mark Incomplete
+                </span>
+              )}
+            </button>
 
             <div className="flex gap-3">
               <button
